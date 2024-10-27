2a7bf4d00ca3ce3efaf42d0e976b22b406165438
Archietheowl <adsg1206@gmail.com>
2024-10-27 17:41:01 +0000

    Sorted folder structure and installed basic dependancies. Also have added bootstrap with a little demo of buttons. Need to share with rest of group at standup.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
-  return <h1>Hello world!</h1>;
-};
-
+  return(
+    <>
+      <h1>Hello world!</h1>
+      <Button variant="primary">Primary</Button>{' '}
+      <Button variant="secondary">Secondary</Button>{' '}
+      <Button variant="success">Success</Button>{' '}
+      <Button variant="warning">Warning</Button>{' '}
+    </>
+  ) };
 export default App;
