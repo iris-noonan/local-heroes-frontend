1fbfad2c712c58e8f356f0d5e21747071ee494de
Archietheowl <adsg1206@gmail.com>
2024-10-28 16:45:42 +0000

    Created jobs pages and added basic nav bar

---
+++ src/pages/JobForm/JobForm.jsx
@@ -0,0 +1,13 @@
+import {useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+
+const JobForm = () => {
+
+    return(
+        <main>
+            <h1>This is the new job form</h1>
+        </main>
+    )
+}
+
+export default JobForm
