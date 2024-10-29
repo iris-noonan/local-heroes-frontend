908055e498ef7fe280d7cb663380dc14978b9491
Iris M <morais.iris@gmail.com>
2024-10-29 13:52:47 +0000

    implemented JWT authentication for sign up and sign in

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,7 +4,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
-function NavBar() {
+function NavBar({ user, handleSignOut }) {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
@@ -12,12 +12,13 @@ function NavBar() {
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
                         <Nav.Link href="/">Home</Nav.Link>
-                        <Nav.Link href="/auth/sign-up">Sign Up</Nav.Link>
-                        <Nav.Link href="/auth/sign-in">Sign In</Nav.Link>
+                        <Nav.Link href="/signup">Sign Up</Nav.Link>
+                        <Nav.Link href="/signin">Sign In</Nav.Link>
                         <Nav.Link href="/jobs">See Jobs</Nav.Link>
                         <Nav.Link href="/jobs/new">Create a Job</Nav.Link>
                         <Nav.Link href="/helpers">See helpers</Nav.Link>
                         <Nav.Link href="/helpers/new">Become a helper</Nav.Link>
+                        <Nav.Link href="" onClick={handleSignOut}>Sign Out</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -8,8 +8,8 @@ const Landing = () => {
         <p>Helping others can help us feel a sense of belonging, make new friends, and connect with our communities.</p>
         <p>Local Heroes is here to help people help themselves by helping others.</p>
         <p>Ask for help, offer help... either way, you’re a local hero!</p>
-        <Button variant="primary">Sign Up</Button>{' '}
-        <Button variant="primary">Sign In</Button>{' '}
+        <a href="/signup" className="btn btn-link" role="button">Sign Up</a>
+        <a href="/signin" className="btn btn-link" role="button">Sign In</a>
       </main>
     </>
   );
