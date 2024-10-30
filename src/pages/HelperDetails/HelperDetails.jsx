70f65d96c08c0517b8aeec6b09c1c0b7a1078902
David <davidmillett@protonmail.com>
2024-10-30 09:40:35 +0000

    show page styling

--- src/pages/HelperDetails/HelperDetails.jsx
+++ src/pages/HelperDetails/HelperDetails.jsx
@@ -1,6 +1,8 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useCallback } from "react"
 import { useParams } from "react-router-dom"
 
+import styles from './HelperDetails.module.scss'
+import profileImage from '../../assets/images/dummy-profile.jpg'
 
 // Services
 import { show } from '../../services/helperService'
@@ -13,32 +15,35 @@ const HelperDetails = () => {
     // Location variables
     const { helperId } = useParams()
 
-    
+    const fetchHelper = useCallback(async () => {
+        try {
+            const { data } = await show(helperId)
+            setHelper(data)
+        } catch (error) {
+            console.log(error)
+        }
+    }, [helperId])
 
     useEffect(() => {
-        const fetchHelper = async () => {
-            try {
-                const { data } = await show(helperId)
-                setHelper(data)
-            } catch (error) {
-                console.log(error)
-            }
-        }
         fetchHelper()
-    }, [helperId])
+    }, [helperId, fetchHelper])
 
     console.log(helper)
 
     return (
-        <main>
-            <section>
-                <p>Profile pic</p>
+        <main className={styles.container}>
+            <header>
+                <img src={profileImage} />
                 {/* <h1>{helper.user.username}</h1> */}
-                <p><strong>Location: </strong></p>
+                <h1>Username</h1>
+                <p><strong>London</strong> - Helper since: 2024</p>
+            </header>
+            <section>
                 {/* <p>{helper.profileDesc}</p> */}
+                <p>Recently retired and suddenly have lots of time on my hands! I love cooking, gardening and odd jobs.</p>
                 {/* <p><strong>Availability:</strong> {helper.availability}</p> */}
                 <p><strong>My skills:</strong> Example, cooking, gardening</p>
-                <p><strong>Helper since: </strong></p>
+                <p><strong>Helper since: </strong>2024</p>
             </section>
         </main>
     )
